ad665486459bbd565826ece951cb358bfb597278
stevensb <agliullin61@gmail.com>
2024-12-20 21:20:20 +0300

    Factorial

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int result = 1;
+    int key = 0;
+
+    cin >> key;
+
+    for(int i = 1; i <= key; i++) {
+        cout << i;
+    }
+}
